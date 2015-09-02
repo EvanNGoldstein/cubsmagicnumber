244b8d967b1027dda4f13860e71cf73e64913c8b
Evan Goldstein <eng2112@columbia.edu>
2015-09-02 05:40:17 -0500

    added last updated timestamp

--- magic_num/magic_num.py
+++ magic_num/magic_num.py
@@ -2,16 +2,36 @@ import requests
 
 def magic_num():
     url = "https://erikberg.com/mlb/standings.json"
+    headers = {
+        "User-agent": "cubsmagicnumber/0.0.0 (eng2112@columbia.edu)"
+    }
 
-    response = requests.get(url).json()
+    teams = {}
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        response = response.json()
+    else:
+        return response.reason
+
 
     for team in response["standing"]:
-        if team["last_name"] == "Cubs":
-            cubs = team
-        if team["last_name"] == "Giants":
-            giants = team
+        teams[team["last_name"]] = (team["won"], team["lost"])
+        # if team["last_name"] == "Cubs":
+        #     cubs = team
+        # if team["last_name"] == "Giants":
+        #     giants = team
 
+    cubs = teams["Cubs"]
+    giants = teams["Giants"]
+    nationals = teams["Nationals"]
     if cubs and giants:
-        magic_num = 163 - int(cubs["won"]) - int(giants["lost"])
+        magic_num_giants = 163 - int(cubs[0]) - int(giants[1])
+        magic_num_nationals = 163 - int(cubs[0]) - int(nationals[1])
+    if magic_num_giants >= magic_num_nationals:
+        magic_num = magic_num_giants
+    else:
+        magic_num = magic_num_nationals
 
-    return (magic_num)
+    return (magic_num, response["standings_date"])
+
--- magic_num/server.py
+++ magic_num/server.py
@@ -1,11 +1,27 @@
 from flask import Flask, render_template
 from magic_num import magic_num
+from datetime import datetime
+from pytz import timezone
 
 app = Flask(__name__)
+cur_magic_num, standings_date = magic_num()
+central_time = 'US/Central'
+last_updated = datetime.now(timezone(central_time))
+
 
 @app.route('/')
 def index():
-    return render_template("index.html", num=magic_num())
+    global last_updated
+    global cur_magic_num
+    global central_time
+    global standings_date
+
+    cur_time = datetime.now(timezone(central_time))
+    if abs(int(cur_time.minute) - int(last_updated.minute)) >= 5:
+        cur_magic_num, standings_date = magic_num()
+        last_updated = cur_time
+
+    return render_template("index.html", num=cur_magic_num, last_updated=standings_date)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", debug=True)
