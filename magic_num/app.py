1e6fb55070df01c7887b86bed655912c925f5f22
Evan Goldstein <eng2112@columbia.edu>
2016-08-22 21:33:58 -0500

    Add index page, Closes #3

--- magic_num/app.py
+++ magic_num/app.py
@@ -7,10 +7,20 @@ app = Flask(__name__)
 standings, standings_date = get_standings()
 central_time = 'US/Central'
 last_updated = datetime.now(timezone(central_time))
+teams = ["Angels", "Astros", "Athletics", "Blue Jays",
+"Braves", "Brewers", "Cardinals", "Cubs", "Diamondbacks",
+"Dodgers", "Giants", "Indians", "Mariners", "Marlins", "Mets",
+ "Nationals", "Orioles", "Padres", "Phillies", "Pirates",
+ "Rangers", "Rays", "Red Sox", "Reds", "Rockies", "Royals",
+  "Tigers", "Twins", "White Sox", "Yankees"]
 
+@app.route('/')
+def index():
+    global teams
+    return render_template("index.html", teams=teams)
 
-@app.route('/<team>')
-def index(team):
+@app.route('/team/<team>')
+def team(team):
     global last_updated, central_time, standings_date, standings
 
     cur_time = datetime.now(timezone(central_time))
@@ -27,7 +37,7 @@ def index(team):
         wc_magic_num = "0. The %s have clinched a playoff berth!" % team
     if(div_magic_num <= 0):
         div_magic_num = "0. The %s have clinched the division!" % team
-    return render_template("index.html", div_num=div_magic_num, wc_num=wc_magic_num, last_updated=standings_date, team=possessive(team))
+    return render_template("team.html", div_num=div_magic_num, wc_num=wc_magic_num, last_updated=standings_date, team=possessive(team))
 
 def possessive(team):
     if team[-1:] in ['S', 's']:
